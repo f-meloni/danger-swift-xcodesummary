db14f2281a3fc5bb30c35a72580a93c36bfde825
Franco Meloni <franco.meloni91@gmail.com>
2019-01-06 13:10:05 +0000

    Rename the project as DangerSwiftXCodeSummary

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "DangerSwiftXCodeSummary",
+    name: "DangerXCodeSummary",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "DangerSwiftXCodeSummary",
-            targets: ["DangerSwiftXCodeSummary"]),
+            name: "DangerXCodeSummary",
+            targets: ["DangerXCodeSummary"]),
     ],
     dependencies: [
         .package(url: "https://github.com/danger/swift", from: "1.0.0"),
@@ -18,10 +18,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "DangerSwiftXCodeSummary",
+            name: "DangerXCodeSummary",
             dependencies: ["Danger"]),
         .testTarget(
-            name: "DangerSwiftXCodeSummaryTests",
-            dependencies: ["DangerSwiftXCodeSummary"]),
+            name: "DangerXCodeSummaryTests",
+            dependencies: ["DangerXCodeSummary"]),
     ]
 )
---
+++ Tests/DangerXCodeSummaryTests/FilePathParserTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+@testable import DangerXCodeSummary
+
+final class FilePathParserTests: XCTestCase {
+    func testItParsesCorrectlyTheLineAndThePath() {
+        let filePath = "/Users/musalj/code/OSS/ObjectiveRecord/Example/SampleProjectTests/FindersAndCreatorsTests.m:111"
+        
+        let result = try? FilePathParser.parseFilePath(filePath: filePath)
+        
+        XCTAssertEqual(result?.path, "/Users/musalj/code/OSS/ObjectiveRecord/Example/SampleProjectTests/FindersAndCreatorsTests.m")
+        XCTAssertEqual(result?.line, 111)
+    }
+    
+    func testItThrowsIfThePathIsInvalid() {
+        let filePath = "/Users/musalj/code/OSS/ObjectiveRecord/Example/SampleProjectTests/FindersAndCreatorsTests.m"
+
+        XCTAssertThrowsError(try FilePathParser.parseFilePath(filePath: filePath))
+    }
+}
