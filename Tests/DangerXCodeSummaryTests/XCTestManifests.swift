b136daaf204ba2191af837cef9754392d1ae91fb
Franco Meloni <franco.meloni91@gmail.com>
2019-01-06 16:02:09 +0000

    Update Linux tests

--- Tests/DangerXCodeSummaryTests/XCTestManifests.swift
+++ Tests/DangerXCodeSummaryTests/XCTestManifests.swift
@@ -1,9 +1,40 @@
 import XCTest
 
+extension CompilerMessageParserTests {
+    static let __allTests = [
+        ("testItParsesCorrectlyACompilerMessage", testItParsesCorrectlyACompilerMessage),
+    ]
+}
+
+extension FilePathParserTests {
+    static let __allTests = [
+        ("testItParsesCorrectlyTheLineAndThePath", testItParsesCorrectlyTheLineAndThePath),
+        ("testItThrowsIfThePathIsInvalid", testItThrowsIfThePathIsInvalid),
+    ]
+}
+
+extension TestFailuresParserTests {
+    static let __allTests = [
+        ("testItParsesAFailedTests", testItParsesAFailedTests),
+    ]
+}
+
+extension XCodeSummaryTests {
+    static let __allTests = [
+        ("testItParsesErrorsCorrectly", testItParsesErrorsCorrectly),
+        ("testItParsesMessagesCorrectly", testItParsesMessagesCorrectly),
+        ("testItParsesWarningsCorrectly", testItParsesWarningsCorrectly),
+        ("testItSendsTheCorrectReportToDanger", testItSendsTheCorrectReportToDanger),
+    ]
+}
+
 #if !os(macOS)
-public func allTests() -> [XCTestCaseEntry] {
+public func __allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(DangerXCodeSummaryTests.allTests),
+        testCase(CompilerMessageParserTests.__allTests),
+        testCase(FilePathParserTests.__allTests),
+        testCase(TestFailuresParserTests.__allTests),
+        testCase(XCodeSummaryTests.__allTests),
     ]
 }
 #endif
