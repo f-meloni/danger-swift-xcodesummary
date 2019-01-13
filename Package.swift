243230d5780be2cafd1e4c3a2419ba356026d311
Franco Meloni <franco.meloni91@gmail.com>
2019-01-13 18:45:54 +0000

    Remove temporary Rocket as SPM dep

--- Package.swift
+++ Package.swift
@@ -13,7 +13,6 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/danger/swift", from: "1.0.0"),
-        .package(url: "https://github.com/f-meloni/rocket", from: "0.4.1") // dev
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
