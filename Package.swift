a8f75b304b36f55ca1df9913f2cf1b57c7d5073e
Franco Meloni <franco.meloni91@gmail.com>
2019-07-23 20:44:39 +0100

    Use Danger 2.0.0

--- Package.swift
+++ Package.swift
@@ -13,9 +13,9 @@ let package = Package(
         .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]) // dev
     ],
     dependencies: [
-        .package(url: "https://github.com/danger/swift", from: "1.0.0"),
+        .package(url: "https://github.com/danger/swift", from: "2.0.0"),
         .package(url: "https://github.com/f-meloni/Rocket", from: "0.4.0"), // dev
-        .package(url: "https://github.com/f-meloni/danger-swift-coverage", from: "0.1.0")
+        //.package(url: "https://github.com/f-meloni/danger-swift-coverage", from: "0.1.0")
     ],
     targets: [
         .target(name: "DangerDependencies",dependencies: ["Danger", "DangerXCodeSummary", "DangerSwiftCoverage"]), //dev
@@ -24,6 +24,6 @@ let package = Package(
             dependencies: ["Danger"]),
         .testTarget(
             name: "DangerXCodeSummaryTests",
-            dependencies: ["DangerXCodeSummary", "DangerSwiftCoverage", "DangerFixtures"]),
+            dependencies: ["DangerXCodeSummary", "DangerFixtures"]),
     ]
 )
