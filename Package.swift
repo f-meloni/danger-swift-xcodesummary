51f78b7f47360565118e3cce184c9d1c1eb6c58d
Franco Meloni <franco.meloni91@gmail.com>
2020-01-12 15:16:26 +0000

    Update danger

--- Package.swift
+++ Package.swift
@@ -13,12 +13,11 @@ let package = Package(
         .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]) // dev
     ],
     dependencies: [
-        .package(url: "https://github.com/danger/swift", from: "2.0.0"),
+        .package(url: "https://github.com/danger/swift", from: "3.0.0"),
         .package(url: "https://github.com/f-meloni/Rocket", from: "1.0.0"), // dev
-        .package(url: "https://github.com/f-meloni/danger-swift-coverage", from: "1.0.0") // dev
     ],
     targets: [
-        .target(name: "DangerDependencies",dependencies: ["Danger", "DangerXCodeSummary", "DangerSwiftCoverage"]), //dev
+        .target(name: "DangerDependencies",dependencies: ["Danger", "DangerXCodeSummary"]), //dev
         .target(
             name: "DangerXCodeSummary",
             dependencies: ["Danger"]),
